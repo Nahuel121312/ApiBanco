950fddd61f10ec526dc618b585962e9eeb2451e8
Nahuel Conde <129802185+Nahuel121312@users.noreply.github.com>
2025-10-23 16:09:49 -0300

    Agrega StatusController

---
+++ src/main/java/com/apiBanco/apiBanco/controlers/StatusController.java
@@ -0,0 +1,28 @@
+package com.apiBanco.apiBanco.controlers;
+
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/status")
+@CrossOrigin(origins = "http://localhost:5173")
+public class StatusController {
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> getStatus(){
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "OK");
+        response.put("timestap", LocalDateTime.now());
+        response.put("message", "API Banco funcionando correctamente");
+        return ResponseEntity.ok(response);
+    }
+}
