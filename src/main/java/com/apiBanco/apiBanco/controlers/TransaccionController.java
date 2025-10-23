57c6bdb11a23be863aa6b119701f4ef72f239a76
Nahuel Conde <129802185+Nahuel121312@users.noreply.github.com>
2025-10-23 16:06:24 -0300

    Actualiza TransaccionController
    Agrega Page, Pageable, PreAuthorize en metodo Delete y modifica en ResponseEntity que retorna el metodo Delete

--- src/main/java/com/apiBanco/apiBanco/controlers/TransaccionController.java
+++ src/main/java/com/apiBanco/apiBanco/controlers/TransaccionController.java
@@ -4,12 +4,15 @@ import com.apiBanco.apiBanco.dtos.transaccion.TransaccionRequestDTO;
 import com.apiBanco.apiBanco.dtos.transaccion.TransaccionResponseDTO;
 import com.apiBanco.apiBanco.models.Cuenta;
 import com.apiBanco.apiBanco.models.Transaccion;
+import com.apiBanco.apiBanco.models.enums.TipoEstado;
 import com.apiBanco.apiBanco.models.enums.TipoTransaccion;
 import com.apiBanco.apiBanco.services.CuentaService;
 import com.apiBanco.apiBanco.services.TransaccionService;
 import jakarta.validation.Valid;
 import lombok.Getter;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,8 +30,13 @@ public class TransaccionController {
     }
 
     @GetMapping
-    public ResponseEntity<List<TransaccionResponseDTO>> listarTransacciones(){
-        List<TransaccionResponseDTO> listarTransacciones = transaccionService.listarTransacciones();
+    public ResponseEntity<Page<TransaccionResponseDTO>> listarTransacciones(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false)TipoTransaccion tipoTransaccion){
+
+        Page<TransaccionResponseDTO> listarTransacciones = transaccionService.listarTransacciones(page, size, tipoTransaccion);
+
         return ResponseEntity.ok(listarTransacciones);
     }
 
@@ -43,10 +51,11 @@ public class TransaccionController {
         return transaccionService.crearTransaccion(transaccionRequest);
    }
 
+    @PreAuthorize("hasRole('ADMIN')")
    @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminarTransaccion(@PathVariable Long id){
         transaccionService.eliminarTransaccion(id);
-            return ResponseEntity.notFound().build();
+        return ResponseEntity.noContent().build();
    }
 
 
