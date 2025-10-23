2d20c57e37d7f528d2630537ca57ba9b134f5390
Nahuel Conde <129802185+Nahuel121312@users.noreply.github.com>
2025-10-23 15:55:19 -0300

    Actualiza ClienteMapper

--- src/main/java/com/apiBanco/apiBanco/mapper/ClienteMapper.java
+++ src/main/java/com/apiBanco/apiBanco/mapper/ClienteMapper.java
@@ -3,10 +3,17 @@ package com.apiBanco.apiBanco.mapper;
 import com.apiBanco.apiBanco.dtos.cliente.ClienteRequestDTO;
 import com.apiBanco.apiBanco.dtos.cliente.ClienteResponseDTO;
 import com.apiBanco.apiBanco.models.Cliente;
+import com.apiBanco.apiBanco.models.enums.Rol;
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
 
 @Component
 public class ClienteMapper {
+
+    private final CuentaMapper cuentaMapper;
+    public ClienteMapper ( CuentaMapper cuentaMapper){
+        this.cuentaMapper = cuentaMapper;}
 
     public Cliente toEntity(ClienteRequestDTO dto){
         Cliente cliente = new Cliente();
@@ -15,6 +22,15 @@ public class ClienteMapper {
         cliente.setEmail(dto.getEmail());
         cliente.setDni(dto.getDni());
         cliente.setDireccion(dto.getDireccion());
+        cliente.setUsername(dto.getUsername());
+        cliente.setTelefono(dto.getTelefono());
+
+        if(dto.getRol() == null){
+            cliente.setRol(Rol.USER);
+        }else {
+            cliente.setRol(dto.getRol());
+        }
+
         return cliente;
     }
 
@@ -26,6 +42,12 @@ public class ClienteMapper {
         dto.setEmail(cliente.getEmail());
         dto.setDni(cliente.getDni());
         dto.setDireccion(cliente.getDireccion());
+        dto.setUsername(cliente.getUsername());
+        dto.setTelefono(cliente.getTelefono());
+
+        if(cliente.getCuenta() != null){
+            dto.setCuentaDto(cuentaMapper.toResponseDTO(cliente.getCuenta()));
+        }
         return dto;
     }
 }
