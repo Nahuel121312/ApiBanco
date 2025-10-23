927733d59939c24fe220dd65003b8317e6a31958
Nahuel Conde <129802185+Nahuel121312@users.noreply.github.com>
2025-10-23 15:53:11 -0300

    Actualiza TarjetaService
    Agrega Page y pageable para listar tarjetas.
    modifica Buscar por ID.
    Modifica seteo de Cuenta en crearTarjeta

--- src/main/java/com/apiBanco/apiBanco/services/TransaccionService.java
+++ src/main/java/com/apiBanco/apiBanco/services/TransaccionService.java
@@ -8,7 +8,11 @@ import com.apiBanco.apiBanco.models.Transaccion;
 import com.apiBanco.apiBanco.models.enums.TipoTransaccion;
 import com.apiBanco.apiBanco.repositories.CuentaRepository;
 import com.apiBanco.apiBanco.repositories.TransaccionRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.cglib.core.Local;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,16 +35,23 @@ public class TransaccionService {
     }
 
     //Listar Transacciones
-    public List<TransaccionResponseDTO> listarTransacciones(){
-        return transaccionRepository.existsByEstadoTrue()
-                .stream().map(transaccionMapper::toResponse)
-                .toList();
+    public Page<TransaccionResponseDTO> listarTransacciones(int page , int size, TipoTransaccion tipoTransaccion){
+        Pageable pageable = PageRequest.of(page, size);
+
+        Page<Transaccion> listaTransaccion;
+        if(tipoTransaccion != null){
+            listaTransaccion = transaccionRepository.findByTipoTransaccionAndEstadoTrue(tipoTransaccion, pageable);
+        }else {
+            listaTransaccion = transaccionRepository.findByEstadoTrue(pageable);
+        }
+
+        return listaTransaccion.map(transaccionMapper::toResponse);
     }
 
     //buscar transaccion por id
     public TransaccionResponseDTO obtenerTransaccion(Long id){
         Transaccion transaccion =  transaccionRepository.findById(id)
-                .orElseThrow(()-> new RuntimeException("Transaccion no encontrada"));
+                .orElseThrow(()-> new EntityNotFoundException("Transaccion con ID:"+ id +" no encontrada"));
 
         return transaccionMapper.toResponse(transaccion);
     }
@@ -49,21 +60,26 @@ public class TransaccionService {
     @Transactional
     public TransaccionResponseDTO crearTransaccion(TransaccionRequestDTO transaccionRequest){
 
-        Cuenta origen = new Cuenta();
-        Cuenta destino = new Cuenta();
+        Cuenta origen = cuentaRepository.findById(transaccionRequest.getIdCuentaOrigen())
+                .orElseThrow(()->new EntityNotFoundException("Cuenta con ID:"+ transaccionRequest.getIdCuentaOrigen() +" no encontrada"));
+        Cuenta destino = cuentaRepository.findById(transaccionRequest.getIdCuentaDestino())
+                .orElseThrow(()-> new EntityNotFoundException("Cuenta con ID:"+ transaccionRequest.getIdCuentaDestino() +" no encontrada"));
 
-        origen.setIdCuenta(transaccionRequest.getIdCuentaOrigen());
-        destino.setIdCuenta(transaccionRequest.getIdCuentaDestino());
 
         Transaccion transaccion = transaccionMapper.toEntity(transaccionRequest);
+
+        if(origen.getSaldo() < transaccion.getMonto()){
+            throw new RuntimeException("Saldo insuficiente");
+        }else {
+            origen.setSaldo(origen.getSaldo() - transaccion.getMonto());
+            destino.setSaldo(destino.getSaldo() + transaccion.getMonto());
+            transaccion.setEnviada(true);
+        }
         transaccion.setFecha(LocalDateTime.now());
         transaccion.setCuentaOrigen(origen);
         transaccion.setCuentaDestino(destino);
         transaccion.setEstado(true);
 
-        if(origen.getSaldo() < transaccion.getMonto()){
-            throw new RuntimeException("Saldo insuficiente");
-        }
 
         transaccionRepository.save(transaccion);
 
@@ -74,8 +90,9 @@ public class TransaccionService {
     //Eliminar Transaccion
     public void eliminarTransaccion(Long id){
         Transaccion transaccion = transaccionRepository.findById(id)
-                .orElseThrow(()-> new RuntimeException("Transaccion no encontrada"));
+                .orElseThrow(()-> new EntityNotFoundException("Transaccion con ID:"+ id +" no encontrada"));
         transaccion.setEstado(false);
+        transaccionRepository.save(transaccion);
     }
 
 
