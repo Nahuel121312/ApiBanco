5576c27c759b283a6d32cd27fb9f4a49bea9a29c
Nahuel Conde <129802185+Nahuel121312@users.noreply.github.com>
2025-10-23 16:09:02 -0300

    Agrega LoginResponse y Request

---
+++ src/main/java/com/apiBanco/apiBanco/dtos/login/LoginRequestDTO.java
@@ -0,0 +1,10 @@
+package com.apiBanco.apiBanco.dtos.login;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class LoginRequestDTO {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/apiBanco/apiBanco/dtos/login/LoginResponseDTO.java
@@ -0,0 +1,10 @@
+package com.apiBanco.apiBanco.dtos.login;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter @AllArgsConstructor
+public class LoginResponseDTO {
+    private String token;
+}
