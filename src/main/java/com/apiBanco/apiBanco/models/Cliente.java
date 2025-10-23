016d5822669815f4fe4f585c3a934bb2e262e56d
Nahuel Conde <129802185+Nahuel121312@users.noreply.github.com>
2025-10-23 15:51:07 -0300

    Actualiza entidad Cliente
    Agrega Rol , Username y password para JWT

--- src/main/java/com/apiBanco/apiBanco/models/Cliente.java
+++ src/main/java/com/apiBanco/apiBanco/models/Cliente.java
@@ -1,5 +1,6 @@
 package com.apiBanco.apiBanco.models;
 
+import com.apiBanco.apiBanco.models.enums.Rol;
 import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.*;
 import lombok.Generated;
@@ -21,10 +22,25 @@ public class Cliente {
 
     private String nombre;
     private String apellido;
+
+    @Column(unique = true)
     private String dni;
+
+    @Column(unique = true)
     private String email;
+
     private String telefono;
     private String direccion;
+
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String password;
+
     private boolean estado;
     @JsonFormat(shape = JsonFormat.Shape.STRING , pattern = "yyyy/MM/dd")
     private LocalDateTime fechaRegistro;
