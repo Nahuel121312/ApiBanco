42370cd100ca658c4bc670508cfa616f78d47dc9
Nahuel Conde <129802185+Nahuel121312@users.noreply.github.com>
2025-10-23 16:06:43 -0300

    Actualiza ClienteService

--- src/main/java/com/apiBanco/apiBanco/services/ClienteService.java
+++ src/main/java/com/apiBanco/apiBanco/services/ClienteService.java
@@ -4,39 +4,48 @@ import com.apiBanco.apiBanco.dtos.cliente.ClienteRequestDTO;
 import com.apiBanco.apiBanco.dtos.cliente.ClienteResponseDTO;
 import com.apiBanco.apiBanco.mapper.ClienteMapper;
 import com.apiBanco.apiBanco.models.Cliente;
-import com.apiBanco.apiBanco.models.Cuenta;
-import com.apiBanco.apiBanco.models.enums.TipoCuenta;
+import com.apiBanco.apiBanco.models.enums.Rol;
 import com.apiBanco.apiBanco.repositories.ClienteRepository;
-import com.apiBanco.apiBanco.repositories.CuentaRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ClienteService {
 
     private final ClienteRepository clienteRepository;
     private final ClienteMapper clienteMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public ClienteService(ClienteRepository clienteRepository, ClienteMapper clienteMapper){
+    public ClienteService(ClienteRepository clienteRepository, ClienteMapper clienteMapper, PasswordEncoder passwordEncoder){
         this.clienteRepository = clienteRepository;
         this.clienteMapper = clienteMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     //Listar clientes
-    public List<ClienteResponseDTO> listarClientes(){
-        return clienteRepository.findByEstadoTrue().stream()
-                .map(clienteMapper::toResponseDTO)
-                .toList();
+    public Page<ClienteResponseDTO> listarClientes(int page, int size, String filtroNombre){
+
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Cliente> clientes;
+        if(filtroNombre != null && !filtroNombre.isEmpty()){
+            clientes = clienteRepository.findByNombreContainingIgnoreCaseAndEstadoTrue(filtroNombre, pageable);
+        }else {
+            clientes = clienteRepository.findByEstadoTrue(pageable);
+        }
+        return clientes.map(clienteMapper::toResponseDTO);
     }
     //Buscar por id
     public ClienteResponseDTO obtenerCliente(Long id){
          Cliente cliente = clienteRepository.findById(id)
-                 .orElseThrow(()-> new RuntimeException("Cliente no encontrado"));
+                 .orElseThrow(()-> new EntityNotFoundException("Cliente con ID: "+ id +" no encontrado"));
          return clienteMapper.toResponseDTO(cliente);
     }
 
@@ -45,6 +54,8 @@ public class ClienteService {
         Cliente cliente = clienteMapper.toEntity(clienteRequest);
         cliente.setFechaRegistro(LocalDateTime.now());
         cliente.setEstado(true);
+        cliente.setPassword(passwordEncoder.encode(clienteRequest.getPassword()));
+        cliente.setRol(Rol.ADMIN);
         clienteRepository.save(cliente);
         return clienteMapper.toResponseDTO(cliente);
     }
@@ -52,13 +63,15 @@ public class ClienteService {
     //Actualizar Cliente
     public ClienteResponseDTO actualizarCliente(Long id, ClienteRequestDTO dto){
         Cliente clienteExistente = clienteRepository.findById(id)
-                .orElseThrow(()-> new RuntimeException("Cliente no encontrado"));
+                .orElseThrow(()-> new EntityNotFoundException("Cliente con ID: "+ id +" no encontrado"));
 
         clienteExistente.setNombre(dto.getNombre());
         clienteExistente.setApellido(dto.getApellido());
         clienteExistente.setEmail(dto.getEmail());
         clienteExistente.setDni(dto.getDni());
         clienteExistente.setDireccion(dto.getDireccion());
+        clienteExistente.setUsername(dto.getUsername());
+
         Cliente actualizado = clienteRepository.save(clienteExistente);
 
         return clienteMapper.toResponseDTO(actualizado);
@@ -67,7 +80,7 @@ public class ClienteService {
     //Eliminar Cliente
     public void eliminarCliente(Long id){
         Cliente cliente = clienteRepository.findById(id)
-                .orElseThrow(()-> new RuntimeException("Cliente no encontrado"));
+                .orElseThrow(()-> new EntityNotFoundException("Cliente con ID: "+ id +" no encontrado"));
         cliente.setEstado(false);
         clienteRepository.save(cliente);
     }
