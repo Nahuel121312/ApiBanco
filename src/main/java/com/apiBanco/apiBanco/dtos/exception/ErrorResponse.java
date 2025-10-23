f66792a4e25686784b507478d877e9674c647471
Nahuel Conde <129802185+Nahuel121312@users.noreply.github.com>
2025-10-23 16:09:23 -0300

    Agrega Error response

--- src/main/java/com/apiBanco/apiBanco/dtos/exception/ErrorResponse.java
+++ src/main/java/com/apiBanco/apiBanco/dtos/exception/ErrorResponse.java
@@ -1,4 +1,23 @@
 package com.apiBanco.apiBanco.dtos.exception;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+
+@Getter @Setter
 public class ErrorResponse {
+    private LocalDateTime timestap;
+    private int status;
+    private String error;
+    private String message;
+
+    public ErrorResponse (LocalDateTime timestap, int status, String error , String message){
+        this.timestap = timestap;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+    }
 }
