214d15a8bc8bc6e4d415213482d02cb5170cc85b
Nahuel Conde <129802185+Nahuel121312@users.noreply.github.com>
2025-10-24 11:10:43 -0300

    Actualiza JwtService
    Agrega "ROLE_" y modifica expiracion del token a 24 horas

--- src/main/java/com/apiBanco/apiBanco/security/JwtService.java
+++ src/main/java/com/apiBanco/apiBanco/security/JwtService.java
@@ -1,11 +1,9 @@
 package com.apiBanco.apiBanco.security;
 
-import com.apiBanco.apiBanco.models.Cliente;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
@@ -23,9 +21,9 @@ public class JwtService {
     public String generarToken(String username, String rol) {
         return Jwts.builder()
                 .setSubject(username)
-                .claim("rol", rol)
+                .claim("rol","ROLE_" + rol)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+ 1000 * 60 * 60))//1 hora
+                .setExpiration(new Date(System.currentTimeMillis()+ 1000 * 60 * 60 * 24))//24 horas
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)// Firma el token
                 .compact();//Devuelve el token final como String
     }
